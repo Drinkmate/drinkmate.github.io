bea0a45aaa4d191c336ed496aefc80f0ecd6dcb7
Paul Kreißig <paulkreissig@web.de>
2025-10-06 17:00:04 +0200

    Patch

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2520197788"
+    serviceWorkerVersion: "1391716301"
   }
 });
